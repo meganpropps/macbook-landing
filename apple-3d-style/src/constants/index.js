168fef2b4210e2a7fb6d0ec43fd14c69e661db83
meganpropps <meganpropps@gmail.com>
2025-12-03 17:37:24 -0500

    first commit

---
+++ apple-3d-style/src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+    {label: 'Store'},
+    {label: 'Mac', href: '/mac'},
+    {label: 'iPhone', href: '/iphone'},
+    {label: 'Apple Watch', href: '/watch'},
+    {label: 'Vision', href: '/airpods'},
+    {label: 'AirPods', href: '/airpods'},
+]
